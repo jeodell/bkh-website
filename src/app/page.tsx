357779f2c33fd8430e4938518637a2698308347e
Jason O'Dell <jason@vitalflohealth.com>
2024-07-30 00:07:58 -0400

    Updating to use next image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,23 @@
+import Image from 'next/image'
 import Card from '../components/Card'
 
 export default function Home() {
   return (
     <>
       <div>
-        <img alt="" src="/images/mountains.jpg" className="h-auto w-full object-cover" />
+        <Image
+          alt="cover photo of mountains"
+          src="/images/mountains.jpg"
+          className="h-auto w-full object-cover"
+          width={1440}
+          height={960}
+        />
       </div>
       <div className="flex items-center justify-between p-24">
-        <Card href="/galleries/drone" title="Drone" body="Drone photos" color="#dbe9f1" />
-        <Card href="/galleries/illustration" title="Illustration" body="Illustration photos" color="#dceae9" />
-        <Card href="/galleries/landscape" title="Landscape" body="Landscape photos" color="#e6eee5" />
-        <Card href="/galleries/portrait" title="Portrait" body="Portrait photos" color="#e9ecf1" />
+        <Card href="/drone" title="Drone" body="Drone photos" color="#dbe9f1" />
+        <Card href="/illustration" title="Illustration" body="Illustration photos" color="#dceae9" />
+        <Card href="/nature" title="Nature" body="Nature photos" color="#e6eee5" />
+        <Card href="/portrait" title="Portrait" body="Portrait photos" color="#e9ecf1" />
       </div>
     </>
   )
