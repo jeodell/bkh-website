942152d16e8d91fdde7dd8cf244c5e6d19f710b9
Jason O'Dell <jason@vitalflohealth.com>
2024-07-30 00:08:09 -0400

    Adding about page layout

---
+++ src/app/about/layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AboutLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return <div className="container mx-auto">{children}</div>
+}
