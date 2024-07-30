f97835edd578359b92e5a5d1273770c47a7561f3
Jason O'Dell <jason@vitalflohealth.com>
2024-07-30 00:09:19 -0400

    Removing unnecessary divs on about page and using next image

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,22 +1,26 @@
+import Image from 'next/image'
+
 export default function About() {
   return (
-    <div className="mx-auto w-2/3 max-w-5xl py-12">
-      <section>
-        <div className="flex flex-wrap">
-          <div className="flex-auto px-4 lg:basis-5/12">
-            <img src="/images/b_laying.jpg" alt="photographer laying on mountainside" className="img-fluid" />
-          </div>
-          <div className="mt-8 flex flex-auto flex-col justify-center gap-y-4 px-4 lg:mt-0 lg:basis-7/12">
-            <p className="text-xl lg:text-lg">
-              You&apos;ve found the hub of my creative outlet. My photography usually connects to exploration, to the
-              earth, and to the human, but with a style that&apos;s still evolving. You never know what exactly
-              you&apos;ll find. I just thank you for looking.
-            </p>
-            <p className="text-xl lg:text-lg">Based in North Carolina.</p>
-            <p className="text-xl lg:text-lg">Any questions? Feel free to contact me!</p>
-          </div>
-        </div>
-      </section>
-    </div>
+    <section className="grid grid-cols-3 justify-center py-12">
+      <div className="col-span-1">
+        <Image
+          src="/images/b_laying.jpg"
+          alt="photographer laying on mountainside"
+          className="img-fluid"
+          width={500}
+          height={333}
+        />
+      </div>
+      <div className="col-span-2 flex flex-col justify-center gap-y-4 px-4">
+        <p className="text-xl lg:text-lg">
+          You&apos;ve found the hub of my creative outlet. My photography usually connects to exploration, to the earth,
+          and to the human, but with a style that&apos;s still evolving. You never know what exactly you&apos;ll find. I
+          just thank you for looking.
+        </p>
+        <p className="text-xl lg:text-lg">Based in North Carolina.</p>
+        <p className="text-xl lg:text-lg">Any questions? Feel free to contact me!</p>
+      </div>
+    </section>
   )
 }
