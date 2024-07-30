141504a22e5e5ad7d7c1c63b0047f64046db5ef8
Jason O'Dell <jason@vitalflohealth.com>
2024-07-30 00:08:50 -0400

    Removing unnecesarry dive on contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -25,60 +25,58 @@ export default function Contact() {
   console.log(watch('message'))
 
   return (
-    <div className="container mx-auto my-16">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="space-y-12">
-          <div className="border-b border-gray-900/10 pb-12">
-            <h2 className="text-center text-xl font-semibold text-gray-900">Contact Form</h2>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="space-y-12">
+        <div className="border-b border-gray-900/10 pb-12">
+          <h2 className="text-center text-xl font-semibold text-gray-900">Contact Form</h2>
 
-            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-              <div className="sm:col-span-4">
-                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                  Email address
-                </label>
-                <div className="mt-2">
-                  <input
-                    {...register('email', { required: true })}
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                  {errors.email && <span>This field is required</span>}
-                </div>
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-4">
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  {...register('email', { required: true })}
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+                {errors.email && <span>This field is required</span>}
               </div>
-              <div className="sm:col-span-6">
-                <label htmlFor="message" className="block text-sm font-medium leading-6 text-gray-900">
-                  Message
-                </label>
-                <div className="mt-2">
-                  <textarea
-                    {...register('message', { required: true })}
-                    id="message"
-                    name="message"
-                    rows={4}
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                  {errors.message && <span>This field is required</span>}
-                </div>
+            </div>
+            <div className="sm:col-span-6">
+              <label htmlFor="message" className="block text-sm font-medium leading-6 text-gray-900">
+                Message
+              </label>
+              <div className="mt-2">
+                <textarea
+                  {...register('message', { required: true })}
+                  id="message"
+                  name="message"
+                  rows={4}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+                {errors.message && <span>This field is required</span>}
               </div>
             </div>
           </div>
         </div>
+      </div>
 
-        <div className="mt-6 flex items-center justify-end gap-x-6">
-          <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
-            Cancel
-          </button>
-          <button
-            type="submit"
-            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Save
-          </button>
-        </div>
-      </form>
-    </div>
+      <div className="mt-6 flex items-center justify-end gap-x-6">
+        <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
+          Cancel
+        </button>
+        <button
+          type="submit"
+          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Save
+        </button>
+      </div>
+    </form>
   )
 }
