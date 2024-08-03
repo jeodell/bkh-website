9f639da0b430e4889a3176946d5a97f101bca10c
Jason O'Dell <jason@vitalflohealth.com>
2024-08-03 15:20:54 -0400

    Updating layout of about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,25 +2,27 @@ import Image from 'next/image'
 
 export default function About() {
   return (
-    <section className="grid grid-cols-3 justify-center py-12">
+    <section className="flex flex-col items-center justify-center gap-8 py-12 md:grid md:grid-cols-3">
       <div className="col-span-1">
-        <Image
-          src="/images/b_laying.jpg"
-          alt="photographer laying on mountainside"
-          className="img-fluid"
-          width={500}
-          height={333}
-          priority
-        />
+        <div className="max-w-96">
+          <Image
+            src="/images/b_laying.jpg"
+            alt="photographer laying on mountainside"
+            className="w-auto"
+            width={500}
+            height={333}
+            priority
+          />
+        </div>
       </div>
-      <div className="col-span-2 flex flex-col justify-center gap-y-4 px-4">
-        <p className="text-xl lg:text-lg">
+      <div className="col-span-2 flex flex-col justify-center gap-y-4">
+        <p className="lg:text-lg">
           You&apos;ve found the hub of my creative outlet. My photography usually connects to exploration, to the earth,
           and to the human, but with a style that&apos;s still evolving. You never know what exactly you&apos;ll find. I
           just thank you for looking.
         </p>
-        <p className="text-xl lg:text-lg">Based in North Carolina.</p>
-        <p className="text-xl lg:text-lg">Any questions? Feel free to contact me!</p>
+        <p className="lg:text-lg">Based in North Carolina.</p>
+        <p className="lg:text-lg">Any questions? Feel free to contact me!</p>
       </div>
     </section>
   )
