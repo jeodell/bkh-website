ea53566d0cb1dfc5f6355c445c7271252e1a2c8d
Jason O'Dell <jason@vitalflohealth.com>
2024-08-04 19:56:34 -0400

    Changing cover photo and adding card photo for portrait

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,17 @@ export default function Home() {
       <div>
         <Image
           alt="cover photo of mountains"
-          src="/images/mountains.jpg"
-          className="h-auto max-h-96 w-full object-cover lg:max-h-[480px]"
+          src="/images/salem_lake.jpg"
+          className="h-auto w-full object-cover"
           width={1440}
           height={960}
           priority
         />
       </div>
-      <div className="container grid max-w-6xl grid-cols-1 gap-8 py-12 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="container grid max-w-7xl grid-cols-1 gap-8 py-12 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-6">
         <Card href="/travel" title="Travel" color="bg-travel" src="/images/travel/cover.jpg" />
         <Card href="/nature" title="Nature" color="bg-nature" src="/images/nature/cover.jpg" />
-        <Card href="/portrait" title="Portrait" color="bg-portrait" src="/images/portrait/cover.jpg" />
+        <Card href="/portrait" title="Portrait" color="bg-portrait" src="/images/portrait/kayak.jpg" />
         <Card href="/drone" title="Drone" color="bg-drone" src="/images/drone/cover.jpg" />
         <Card href="film" title="Film" color="bg-film" src="/images/film/cover.jpg" />
         <Card href="/design" title="Design" color="bg-design" src="/images/design/cover.jpg" />
