59f5eb511050c5be821cc78d2bfe35e5e80fef0a
Jason O'Dell <jason@vitalflohealth.com>
2024-08-04 19:57:08 -0400

    Updating image links

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -23,7 +23,21 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const photoLinks: { [key: string]: { src: string; alt: string }[] } = {
-  portrait: [{ src: 'chef.jpg', alt: 'chefmannnnn' }],
+  portrait: [
+    { src: 'canyon_wedding1.jpg', alt: 'bride and groom standing on rock' },
+    { src: 'canyon_wedding2.jpg', alt: 'bride and groom walking down aisle' },
+    { src: 'canyon_wedding3.jpg', alt: 'bride and groom in front of river' },
+    { src: 'engagement.jpg', alt: 'couple kissing in front of lake' },
+    { src: 'kayak.jpg', alt: 'woman in kayak' },
+    { src: 'lake_couple.jpg', alt: 'couple hugging in front of lake' },
+    { src: 'maternity1.jpg', alt: 'maternity photo in field' },
+    { src: 'maternity2.jpg', alt: 'maternity photo in field' },
+    { src: 'movement.jpg', alt: 'woman smiling and flipping hair' },
+    { src: 'newborn1.jpg', alt: 'newborn baby in blanket' },
+    { src: 'newborn2.jpg', alt: 'mom and dad holding newborn' },
+    { src: 'river_baby.jpg', alt: 'mom and baby playing in river' },
+    { src: 'veil.jpg  ', alt: 'woman under veil' },
+  ],
   nature: [
     { src: 'b_laying.jpg', alt: 'woman laying in field by mountain' },
     {
